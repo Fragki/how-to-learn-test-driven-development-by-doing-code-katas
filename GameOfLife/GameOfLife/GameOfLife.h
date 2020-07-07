45200b02787be31a02ca2da7c7403d55a29e59fc
Fragi <fragi@trikalabs.com>
2020-07-07 23:32:08 +0100

    Game of life code kata

---
+++ GameOfLife/GameOfLife/GameOfLife.h
@@ -0,0 +1,19 @@
+//
+//  GameOfLife.h
+//  GameOfLife
+//
+//  Created by FRAGKISKOS KATSIMPAS on 07/07/2020.
+//  Copyright © 2020 FRAGKISKOS KATSIMPAS. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GameOfLife.
+FOUNDATION_EXPORT double GameOfLifeVersionNumber;
+
+//! Project version string for GameOfLife.
+FOUNDATION_EXPORT const unsigned char GameOfLifeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GameOfLife/PublicHeader.h>
+
+
---
+++ GameOfLife/GameOfLifeTests/GameOfLifeTests.swift
@@ -0,0 +1,89 @@
+//
+//  GameOfLifeTests.swift
+//  GameOfLifeTests
+//
+//  Created by FRAGKISKOS KATSIMPAS on 07/07/2020.
+//  Copyright © 2020 FRAGKISKOS KATSIMPAS. All rights reserved.
+//
+
+import XCTest
+@testable import GameOfLife
+
+class GameOfLifeTests: XCTestCase {
+
+    func test_initial_state() {
+        let randomBoard = [[0, 1, 0], [0, 0, 0], [1, 1 , 1]]
+        let game = Game(board:randomBoard)
+        
+        let board = game.getBoard()
+        
+        XCTAssertEqual(randomBoard, board)
+    }
+    
+    func test_nextGeneration_cellIsAlive_0AliveNeighbours_theCellDies(){
+        let initialSeed = [[0, 0, 0], [0, 1, 0], [0, 0 , 0]] // Our cell of interest is the (1,1)
+        let game = Game(board:initialSeed)
+        
+        game.nextGeneration()
+        
+        let expectedBoard = [[0, 0, 0], [0, 0, 0], [0, 0 , 0]]
+        assertExpectedGameBoard(expectedBoard: expectedBoard, game: game)
+    }
+    
+    func test_nextGeneration_cellIsAlive_1AliveNeighbours_theCellDies(){
+        let initialSeed = [[0, 0, 0], [0, 1, 0], [1, 0 , 0]] // Our cell of interest is the (1,1)
+        let game = Game(board:initialSeed)
+        
+        game.nextGeneration()
+        
+        let expectedBoard = [[0, 0, 0], [0, 0, 0], [0, 0 , 0]]
+        assertExpectedGameBoard(expectedBoard: expectedBoard, game: game)
+    }
+
+    func test_nextGeneration_cellIsAlive_2AliveNeighbours_theCellRemainAlive(){
+        let initialSeed = [[0, 0, 1], [0, 1, 0], [1, 0 , 0]] // Our cell of interest is the (1,1)
+        let game = Game(board:initialSeed)
+        
+        game.nextGeneration()
+        
+        let expectedBoard = [[0, 0, 0], [0, 1, 0], [0, 0 , 0]]
+        assertExpectedGameBoard(expectedBoard: expectedBoard, game: game)
+    }
+    
+    func test_nextGeneration_cellIsAlive_4AliveNeighbours_theCellDies(){
+        let initialSeed = [[0, 1, 1], [1, 1, 0], [1, 0 , 0]] // Our cell of interest is the (1,1)
+        let game = Game(board:initialSeed)
+        
+        game.nextGeneration()
+        
+        let board = game.getBoard()
+        XCTAssertEqual(0, board[1][1])
+    }
+    
+    func test_nextGeneration_cellIsAlive_4OrMoreAliveNeighbours_theCellDies(){
+        let initialSeed = [[1, 1, 1], [1, 1, 1], [1, 0 , 0]] // Our cell of interest is the (1,1)
+        let game = Game(board:initialSeed)
+        
+        game.nextGeneration()
+        
+        let board = game.getBoard()
+        XCTAssertEqual(0, board[1][1])
+    }
+    
+    func test_nextGeneration_cellIsDead_3AliveNeighbours_theCellBecomesAlive(){
+        let initialSeed = [[0, 0, 1], [0, 0, 1], [1, 0 , 0]] // Our cell of interest is the (1,1)
+        let game = Game(board:initialSeed)
+        
+        game.nextGeneration()
+        
+        let board = game.getBoard()
+        XCTAssertEqual(1, board[1][1])
+    }
+    
+    private func assertExpectedGameBoard(expectedBoard: [[Int]], game: Game) {
+        let board = game.getBoard()
+        XCTAssertEqual(expectedBoard, board)
+    }
+
+}
+
